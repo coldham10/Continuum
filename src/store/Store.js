39a88153463f9da720ca7b650f213e94ff0dc2a5
Christopher Oldham <coldham10@gmail.com>
2020-11-28 14:12:51 -0500

    When rehydrating, all habits are re-selected

--- src/store/Store.js
+++ src/store/Store.js
@@ -7,21 +7,31 @@ import {
 import AsyncStorage from '@react-native-community/async-storage';
 import positiveReducer from './reducers/PositiveSlice';
 import negativeReducer from './reducers/NegativeSlice';
-import {persistReducer} from 'redux-persist';
+import {persistReducer, createTransform} from 'redux-persist';
 
 const reducers = combineReducers({
   positiveList: positiveReducer,
   negativeList: negativeReducer,
 });
 
+const selectAllOnRehydrate = createTransform(
+  null,
+  (state) => {
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.forEach((habit) => (habit.selected = true));
+    return newState;
+  },
+  {whitelist: ['positiveList', 'negativeList']},
+);
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  transforms: [selectAllOnRehydrate],
 };
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const immutableInvariantMiddleware = createImmutableStateInvariantMiddleware();
-
-const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
   reducer: persistedReducer,
