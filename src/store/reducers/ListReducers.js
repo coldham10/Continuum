1f9cb97856495b7833f44169dbb29dc6b4795ed5
Christopher Oldham <coldham10@gmail.com>
2020-11-30 12:32:33 -0500

    Consolidated positive and negative reducers to be DRYer

---
+++ src/store/reducers/ListReducers.js
@@ -0,0 +1,59 @@
+/*Single definition of reducers in positiveSlice and negativeSlice*/
+
+import toggleActivity from './ToggleActivity';
+import newHabit from './NewHabit';
+import recomputeHistory from './ReHistory';
+import extendHabit from './ExtendHabit';
+
+export default (positive) => {
+  return {
+    add: (state) => {
+      state.push(newHabit(positive));
+    },
+    remove: (state, action) => {
+      if (action.payload === -1) {
+        return state.slice(0, state.length - 1);
+      }
+      return state.filter((habit) => habit.id !== action.payload);
+    },
+    toggle: (state, action) => {
+      //Toggle and re-calculate (downstream) history and momentum
+      let index = state.findIndex((habit) => habit.id === action.payload.id);
+      if (index !== -1) {
+        let toggled = toggleActivity(state[index], action.payload.date);
+        state[index] = recomputeHistory(toggled.data, toggled.index, positive);
+      }
+    },
+    edit: (state, action) => {
+      //Change title and parameters of habit. Recalculate values (TODO: if necessary)
+      let index;
+      if (action.payload.id === -1) {
+        index = state.length - 1;
+      } else {
+        index = state.findIndex((habit) => habit.id === action.payload.id);
+      }
+      if (index !== -1) {
+        state[index].title = action.payload.title;
+        state[index].parameters = action.payload.params;
+        state[index] = recomputeHistory(state[index], 0, positive);
+      }
+    },
+    toggleSelected: (state, action) => {
+      //Toggle if a habit is shown in the overview
+      let index = state.findIndex((habit) => habit.id === action.payload);
+      if (index !== -1) {
+        state[index].selected = !state[index].selected;
+      }
+    },
+    selectAll: (state) => {
+      state.forEach((habit) => (habit.selected = true));
+    },
+    deselectAll: (state) => {
+      state.forEach((habit) => (habit.selected = false));
+    },
+    extendAll: (state) => {
+      let newState = state.map((habit) => extendHabit(habit));
+      return newState;
+    },
+  };
+};
--- src/store/reducers/PositiveSlice.js
+++ src/store/reducers/PositiveSlice.js
@@ -1,63 +1,11 @@
 import {createSlice} from '@reduxjs/toolkit';
-import newHabit from './NewHabit';
-import toggleActivity from './ToggleActivity';
-import recomputeHistory from './ReHistory';
-import extendHabit from './ExtendHabit';
+import listReducers from './ListReducers';
 
 export const positiveSlice = createSlice({
   name: 'positive',
   //{type: "positive/add", payload: ...} e.g.
   initialState: [],
-  reducers: {
-    add: (state) => {
-      state.push(newHabit(true));
-    },
-    remove: (state, action) => {
-      if (action.payload === -1) {
-        return state.slice(0, state.length - 1);
-      }
-      return state.filter((habit) => habit.id !== action.payload);
-    },
-    toggle: (state, action) => {
-      //Toggle and re-calculate (downstream) history and momentum
-      let index = state.findIndex((habit) => habit.id === action.payload.id);
-      if (index !== -1) {
-        let toggled = toggleActivity(state[index], action.payload.date);
-        state[index] = recomputeHistory(toggled.data, toggled.index, true);
-      }
-    },
-    edit: (state, action) => {
-      //Change title and parameters of habit. Recalculate values (TODO: if necessary)
-      let index;
-      if (action.payload.id === -1) {
-        index = state.length - 1;
-      } else {
-        index = state.findIndex((habit) => habit.id === action.payload.id);
-      }
-      if (index !== -1) {
-        state[index].title = action.payload.title;
-        state[index].parameters = action.payload.params;
-        state[index] = recomputeHistory(state[index], 0, true);
-      }
-    },
-    toggleSelected: (state, action) => {
-      //Toggle if a habit is shown in the overview
-      let index = state.findIndex((habit) => habit.id === action.payload);
-      if (index !== -1) {
-        state[index].selected = !state[index].selected;
-      }
-    },
-    selectAll: (state) => {
-      state.forEach((habit) => (habit.selected = true));
-    },
-    deselectAll: (state) => {
-      state.forEach((habit) => (habit.selected = false));
-    },
-    extendAll: (state) => {
-      let newState = state.map((habit) => extendHabit(habit));
-      return newState;
-    },
-  },
+  reducers: listReducers(true),
 });
 
 export const {
@@ -67,7 +15,8 @@ export const {
   edit,
   toggleSelected,
   selectAll,
-  deselcetAll,
+  deselectAll,
+  extendAll,
 } = positiveSlice.actions;
 
 export default positiveSlice.reducer;
