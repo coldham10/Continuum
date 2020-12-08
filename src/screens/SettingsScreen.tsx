98774052f88017b03bbdeaa653c5f5a26487558f
Christopher Oldham <coldham10@gmail.com>
2020-12-08 13:33:52 +0000

    Propper interaction for attempted background change with no premium. Locked overlays for unavailable backgrounds

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import {connect} from 'react-redux';
 import {imageList} from '../utils/Constants';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function SettingsScreen(props) {
   return (
@@ -49,12 +50,28 @@ function SettingsScreen(props) {
         <FlatList
           horizontal
           data={imageList}
-          renderItem={({item, index}) => (
-            <BackgroundCard
-              source={item}
-              onSelect={props.chooseBackground.bind(this, props.premium, index)}
-            />
-          )}
+          renderItem={({item, index}) => {
+            let type;
+            if (index === props.backgroundSelected) {
+              type = 'SELECTED';
+            } else if (props.premium) {
+              type = 'UNSELECTED';
+            } else {
+              type = 'UNAVAILABLE';
+            }
+            return (
+              <BackgroundCard
+                source={item}
+                type={type}
+                onSelect={props.chooseBackground.bind(
+                  this,
+                  props.premium,
+                  index,
+                )}
+                navigation={props.navigation}
+              />
+            );
+          }}
           style={styles.cardList}
         />
       </View>
@@ -65,15 +82,63 @@ function SettingsScreen(props) {
 function BackgroundCard(props) {
   return (
     <View style={styles.cardContainer}>
-      <TouchableOpacity onPress={() => props.onSelect()}>
-        <Image style={styles.cardImage} source={props.source} />
-      </TouchableOpacity>
+      <Overlay
+        type={props.type}
+        navigation={props.navigation}
+        onSelect={props.onSelect}
+      />
+      <Image style={styles.cardImage} source={props.source} />
     </View>
   );
 }
 
+function Overlay(props) {
+  if (props.type === 'UNAVAILABLE') {
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          props.navigation.navigate('GetPremium', {
+            reason: 'Changing the background',
+          })
+        }
+        style={{
+          ...styles.cardOverlay,
+          backgroundColor: '#5558',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Ionicons name="md-lock" size={50} />
+      </TouchableOpacity>
+    );
+  } else if (props.type === 'SELECTED') {
+    return (
+      <TouchableOpacity style={{...styles.cardOverlay, alignItems: 'flex-end'}}>
+        <View
+          style={{
+            backgroundColor: '#eee8',
+            aspectRatio: 1,
+            padding: 1,
+            borderRadius: 30,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Ionicons name="ios-checkmark" size={30} />
+        </View>
+      </TouchableOpacity>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={styles.cardOverlay}
+        onPress={() => props.onSelect()}
+      />
+    );
+  }
+}
+
 const mapStateToProps = (state, ownProps) => ({
   premium: state.settings.premium,
+  backgroundSelected: state.settings.background,
 });
 const mapDispatchToProps = (dispatch, ownProps) => ({
   setPremium: (isFree) => {
@@ -115,6 +180,13 @@ const styles = StyleSheet.create({
   text: {fontSize: 18, color: '#222', marginLeft: 15},
   cardList: {flexGrow: 0},
   cardContainer: {margin: 10},
-  cardOverlay: {},
-  cardImage: {resizeMode: 'contain', width: 120, height: 240},
+  cardOverlay: {
+    width: 120,
+    height: 180,
+    position: 'absolute',
+    zIndex: 1,
+    padding: 10,
+  },
+
+  cardImage: {resizeMode: 'contain', width: 120, height: 180},
 });
